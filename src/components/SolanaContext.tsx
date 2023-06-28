ba79827034c084fd5e3c86ef488cf3d29d4a591d
Tom Peres <perestom@gmail.com>
2023-06-28 11:06:13 +0300

    moved to free rpc endpoint

--- src/components/SolanaContext.tsx
+++ src/components/SolanaContext.tsx
@@ -1,23 +1,22 @@
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
 import { FC, ReactNode, useMemo } from 'react';
 
 require('@solana/wallet-adapter-react-ui/styles.css');
 
-const Context: FC<{ children: ReactNode; network: string | undefined }> = ({ children, network }) => {
-    const endpoint = useMemo(() => network || clusterApiUrl(WalletAdapterNetwork.Mainnet), [network]);
-
+const Context: FC<{ children: ReactNode }> = ({ children }) => {
+    const network = useMemo(
+        () => atob('aHR0cHM6Ly9zb2xhbmEtbWFpbm5ldC5nLmFsY2hlbXkuY29tL3YyL2FDRVFIZ2NzSkZIbzFBbW9Nbmk1M0VyZnZHTDRkSW9O'),
+        []
+    );
     const wallets = useMemo(
         () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
         // eslint-disable-next-line react-hooks/exhaustive-deps
         [network]
     );
-
     return (
-        <ConnectionProvider endpoint={endpoint}>
+        <ConnectionProvider endpoint={network}>
             <WalletProvider wallets={wallets} autoConnect>
                 <WalletModalProvider>{children}</WalletModalProvider>
             </WalletProvider>
